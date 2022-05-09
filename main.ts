d77edea6f6768e3ba1aae23589b156acfff65294
GGhuter05 <104902141+GGhuter05@users.noreply.github.com>
2022-05-09 16:56:08 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    player.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    player.move(1)
+})
+let player: game.LedSprite = null
+player = game.createSprite(2, 4)
+player.set(LedSpriteProperty.Blink, 500)
+let enemy = game.createSprite(2, 0)
+enemy.set(LedSpriteProperty.Blink, 500)
+basic.forever(function () {
+    enemy.change(LedSpriteProperty.Y, 1)
+    basic.pause(1000)
+    if (enemy.isTouching(player)) {
+        enemy.change(LedSpriteProperty.Y, -4)
+        enemy.change(LedSpriteProperty.X, randint(0, 4))
+    }
+})
